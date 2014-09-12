b41849f6ef0f4899ceb9fdd74cabd5fd5781a038
Evans Hall <msuech87@gmail.com>
2014-09-11 22:16:38 -0500

    Adding a few more view with arguments
    Writing some really basic views that currently don’t really do what
    they are supposed to

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -2,5 +2,12 @@ from django.conf.urls import url
 from polls import views
 
 urlpatterns = [
+    #example pattern : /polls/
     url(r'^$', views.index, name='index'),
+    #example pattern : /polls/3/
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    #example pattern : /polls/3/results/
+    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    #example pattern : /polls/3/vote/
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,3 +3,13 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("You've made it to the polls homepage!")
+
+def detail(request, question_id):
+    return HttpResponse("This is question : %s" % question_id)
+
+def results(request, question_id):
+    return HttpResponse("Poll results for question : %s" % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You are voting on question : %s" % question_id)
+
